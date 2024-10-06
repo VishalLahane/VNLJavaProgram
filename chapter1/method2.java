c8edd78a9b1e41f31572e336d0d093f8a6092ad3
vishal lahane <vlahane782@gmail.com>
2024-10-06 09:30:32 +0530

    added inheritance files

---
+++ chapter1/method2.java
@@ -0,0 +1,13 @@
+public class method2 {
+        public static void main(String[] args){
+            Main15 obj15 = new Main15();
+            System.out.println(obj15.substract(2, 5));
+        }
+
+    }
+
+    class Main15{
+        public int substract(int a, int b){
+            return a-b;
+        }
+    }
